be6490d7bfd2d13c91d538b97c378efa126727a1
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-06-11 13:35:16 +0200

    Add initial

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import sass from "rollup-plugin-sass";
+import commonjs from "rollup-plugin-commonjs";
+import copy from "rollup-plugin-copy";
+
+import packageJson from "./package.json";
+
+export default {
+    input: "src/index.ts",
+    output: [
+        {
+            file: packageJson.main,
+            format: "cjs",
+            sourcemap: true
+        },
+        {
+            file: packageJson.module,
+            format: "esm",
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        peerDepsExternal(),
+        sass({
+            // Prefix global scss. Useful for variables and mixins.
+            output: 'build/bundle.css',
+            insert: true,
+            watch: 'src/components/**/*.scss',
+        }),
+        resolve(),
+        commonjs(),
+        typescript({ useTsconfigDeclarationDir: true })
+    ]
+};
---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import {ITestComponentProps} from "./TestComponent.types";
+import "./TestComponent.scss";
+
+export const TestComponent: React.FC<ITestComponentProps> = ({theme}) => (
+    <div
+        data-testid="test-component"
+        className={`test-component test-component-${theme}`}
+    >
+        <h1 className="heading">I'm the test component</h1>
+        <h2>Made with love by Harvey</h2>
+    </div>
+);
