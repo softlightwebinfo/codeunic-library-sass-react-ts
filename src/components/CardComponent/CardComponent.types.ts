2bafe61a6832484e7d39443205aa67088acffce7
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-07-05 23:57:07 +0200

    Add onClick card

--- src/components/CardComponent/CardComponent.types.ts
+++ src/components/CardComponent/CardComponent.types.ts
@@ -5,7 +5,7 @@ import {IVariant} from "../../Interfaces/IVariant";
 import {IOnClick} from "../../Interfaces/IOnClick";
 
 export interface ICardComponentProps {
-    onClick: (e: IOnClick) => void;
+    onClick?: (e: IOnClick) => void;
     rounded?: boolean;
     style?: IStyle;
     variant?: IVariant;
